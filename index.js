f1c029517e4fde78e2ba8d0be6cfecfc146dfe1a
Aditya Gupta <adityaguptady@gmail.com>
2025-08-29 20:36:15 +0530

    Javascript examples

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello wrold!")
+//"use strict";
+console.log(6)
+
+// console.info("This is information")
+
+// console.error("This is information")
+
+//console = 10
+
+// let height = 165
+// console.log(height)
+
+// let height = 165
+// console.log(height)
+
+var length = 155
+console.log(length)
+
+var length = 210
+console.log(length)
+
+length = 1000
+console.log(length)
+
+let height = 180
+let anotherHeight = height
+console.log(height) // -> 180
+console.log("height")   //height
+
+console.log("height:"+height)
+
+console.log(anotherHeight) // -> 180
+
+let weight      //Declaration
+
+weight = 100    //initialisation
+console.log(weight)     //accessing the variable
+
+count = 10
+console.log(count)
+count = count + 100
+console.log(count)
+
+console.log(weight)
+weight = "100"
+console.log(weight)
+
+weight = weight + count
+console.log(weight)
+console.log(count + weight)
