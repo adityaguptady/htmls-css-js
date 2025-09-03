79bbd7a3440d435fb84f98b90739cc2f9a12c20e
Aditya Gupta <adtyagupta72@gmail.com>
2025-09-03 20:22:38 +0530

    Data types examples

--- index.js
+++ index.js
@@ -196,13 +196,38 @@ console.log(6)
 // console.log(x); // -> 9000
 // console.log(y); // -> 0.00123
 
-let a = 1 / 0
-let b = -Infinity
-console.log(a) // -> Infinity
-console.log(b) // -> -Infinity
-console.log(typeof a) // -> number
-console.log(typeof b) // -> number
-let s = "it's definitely not a number"
-let n = s * 10
-console.log(n) // -> NaN
-console.log(typeof n) // -> number
+// let a = 1 / 0
+// let b = -Infinity
+// console.log(a) // -> Infinity
+// console.log(b) // -> -Infinity
+// console.log(typeof a) // -> number
+// console.log(typeof b) // -> number
+// let s = "it's definitely not a number"
+// let n = s * 10
+// console.log(n) // -> NaN
+// console.log(typeof n) // -> number
+
+
+// let big = 1234567890000000000000n
+// let big2 = 1n
+// console.log(big) // -> 1234567890000000000000n
+// console.log(typeof big) // -> bigint
+// console.log(big2) // -> 1n
+// console.log(typeof big2) // -> bigint
+// console.log(7n / 4n); // -> 1n
+
+// let big3 = 1000n + 20
+
+// let big4 = 1000n / 0n
+
+let country = "Malawi"
+let continent = 'Africa'
+console.log(country); // -> Malawi
+console.log(typeof country); // -> string
+console.log(continent); // -> Africa
+console.log(typeof continent); // -> string
+
+let dialog = "He said then 'How are you?' and the scene ended!"
+let dialog1 = 'He said then "How are you?" and the scene ended!'
+console.log(dialog)
+console.log(dialog1)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,23 @@
-number
+let a = 1 / 0
+// let b = -Infinity
+// console.log(a) // -> Infinity
+// console.log(b) // -> -Infinity
+// console.log(typeof a) // -> number
+// console.log(typeof b) // -> number
+// let s = "it's definitely not a number"
+// let n = s * 10
+// console.log(n) // -> NaN
+// console.log(typeof n) // -> number
+
+
+// let big = 1234567890000000000000n
+// let big2 = 1n
+// console.log(big) // -> 1234567890000000000000n
+// console.log(typeof big) // -> bigint
+// console.log(big2) // -> 1n
+// console.log(typeof big2) // -> bigint
+// console.log(7n / 4n); // -> 1n
+
+// let big3 = 1000n + 20
+
+// let big4 = 1000n / 0n
