daca065d6d5179210957a579bf828f2d4d873f75
Aditya Gupta <adityaguptady@gmail.com>
2025-09-17 20:05:24 +0530

    added String compound operator

--- index.js
+++ index.js
@@ -709,10 +709,16 @@ console.log(6)
 // b ||= true      // -> b = b || true
 // console.log(b)
 
-let greetings = "Hi";
-console.log(greetings + " " + "Alice"); // -> Hi Alice
-let sentence = "Happy New Year ";
-let newSentence = sentence + 10191;
-console.log(newSentence); // -> Happy New Year 10191
-console.log(typeof newSentence); // -> string
+// let greetings = "Hi";
+// console.log(greetings + " " + "Alice"); // -> Hi Alice
+// let sentence = "Happy New Year ";
+// let newSentence = sentence + 10191;
+// console.log(newSentence); // -> Happy New Year 10191
+// console.log(typeof newSentence); // -> string
+
+let sentence = "Happy New ";
+sentence += "Year ";
+sentence += 10191;
+console.log(sentence); // -> Happy New Year
+
 
