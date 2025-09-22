ecb02bba3eecef94d0d7bde16f5c5771fac0d40e
Aditya Gupta <adityaguptady@gmail.com>
2025-09-22 08:08:21 +0530

    operators

--- index.js
+++ index.js
@@ -716,9 +716,78 @@ console.log(6)
 // console.log(newSentence); // -> Happy New Year 10191
 // console.log(typeof newSentence); // -> string
 
-let sentence = "Happy New ";
-sentence += "Year ";
-sentence += 10191;
-console.log(sentence); // -> Happy New Year
+// let sentence = "Happy New ";
+// sentence += "Year ";
+// sentence += 10191;
+// console.log(sentence); // -> Happy New Year
 
+// console.log(10 === 5); // -> false
+// console.log(10 === 10); // -> true
+// console.log(10 === 10n); // -> false
+// console.log(10 === "10"); // -> false
+// console.log("10" === "10"); // -> true
+// console.log("Alice" === "Bob"); // -> false
+// console.log(0 === false); // -> false
+// console.log(undefined === false); // -> false
 
+// console.log(10 == 5); // -> false
+// console.log(10 == 10); // -> true
+// console.log(10 == 10n); // -> true
+// console.log(10 == "10"); // -> true
+// console.log("10" == "10"); // -> true
+// console.log("Alice" == "Bob"); // -> false
+// console.log(0 == false); // -> true
+// console.log(undefined == false) // -> false
+// console.log(NaN == NaN); // -> false
+
+// console.log(10 !== 5); // -> true
+// console.log(10 !== 10); // -> false
+// console.log(10 !== 10n); // -> true
+// console.log(10 !== "10"); // -> true
+// console.log("10" !== "10"); // -> false
+// console.log("Alice" !== "Bob"); // -> true
+// console.log(0 !== false); // -> true
+// console.log(undefined !== false); // -> true
+
+// console.log(10 != 5); // -> true
+// console.log(10 != 10); // -> false
+// console.log(10 != 10n); // -> false
+// console.log(10 != "10"); // -> false
+// console.log("10" != "10"); // -> false
+// console.log("Alice" != "Bob"); // -> true
+// console.log(0 !=  false); // -> false
+// console.log(undefined != false); // -> true
+// console.log(NaN == NaN)
+// console.log(NaN != NaN)
+
+// console.log(10 > 100); // -> false
+// console.log(101 > 100); // -> true
+// console.log(101 > "100"); // -> true
+// console.log(101 < 100); // -> false
+// console.log(100n < 102); // -> true
+// console.log("10" < 20n); // -> true
+// console.log(101 <= 100); // -> false
+// console.log(10 >= 10n); // -> true
+// console.log("10" <=  20); // -> true
+
+// console.log("b" > "a"); // -> true
+// console.log("a" > "B"); // -> true
+// console.log("B" > "A"); // -> true
+// console.log("A" > "4"); // -> true
+// console.log("4" > "1"); // -> true
+// console.log("ab1" < "ab4"); // -> true
+// console.log("ab4" < "abA"); // -> true
+// console.log("abB" < "aba"); // -> true
+// console.log("aba" < "abb"); // -> true
+// console.log("ab" < "ab4"); // -> true
+
+let names = ["Patti", "Bob"];
+let name = names[0];
+console.log(names instanceof Array); // -> true
+console.log(name instanceof Array); // -> false
+
+console.log(true ? "Alice" : "Bob"); // -> Alice
+console.log(false ? "Alice" : "Bob"); // -> Bob
+
+console.log(1<10 ? "Alice" : "Bob"); // -> Alice
+console.log(1>10 ? "Alice" : "Bob"); // -> Bob
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-a &&= fals
+Bob
