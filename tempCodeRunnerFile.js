220f2e26101f144ef96df8c15f3c16070c426932
Aditya Gupta <adtyagupta72@gmail.com>
2025-09-25 19:53:03 +0530

    if examples

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-Bob
+// if(userAge > 21)
+// { 
+//     if (cartValue >= 300 || points >= 500) 
+//     { 
+//         shippingCost = 0
+//     }
+// } 
--- userInteraction.js
+++ userInteraction.js
@@ -26,3 +26,36 @@ function promptDialogExample()
     let age = prompt("Hello " + name + ", how old are you?")
     alert(name + " is " + age + " years old")
 }
+
+// let isUserReady = confirm("Are you ready?")
+// console.log(isUserReady)
+// if(isUserReady)
+// {
+//     alert("User ready!")
+// }
+
+// let unitPrice = 10
+// let pieces = prompt("How many pieces do you order?", 0);
+// if (pieces > 0) 
+// { 
+//     let total = unitPrice * pieces
+//     console.log(total)
+// }
+// console.log(total)
+
+let userAge = prompt("Enter your age:")
+let isFemale = false
+let points = 703
+let cartValue = 299
+let shippingCost = 9.99
+// if(userAge > 21)
+// { 
+//     if (cartValue >= 300 || points >= 500) 
+//     { 
+//         shippingCost = 0
+//     }
+// } 
+if(userAge > 21 && (cartValue >= 300 || points >= 500))
+    shippingCost = 0
+alert("Shipping cost: "+shippingCost)
+
