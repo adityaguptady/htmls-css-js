00812102ae5675bf7552978d7d1d15c3d1a5eba6
Aditya Gupta <adityaguptady@gmail.com>
2025-12-24 09:42:16 +0530

    Class expression

--- userInteraction.js
+++ userInteraction.js
@@ -1950,27 +1950,69 @@ returns => 5 * 24
 // console.log("weight: ", weight)
 // console.log("fuelType: ", fuelType)
 
-let Vehicle = function({id, latitude, longitude})
+// let Vehicle = function({id, latitude, longitude})
+// {
+//     this.setPosition = function(latitude, longitude) 
+//     {
+//         this.time = Date.now();
+//         this.longitude = longitude;
+//         this.latitude = latitude;
+//     };
+
+//     this.getPosition = function() 
+//     {
+//         return {latitude: this.latitude, longitude: this.longitude}
+//     };
+
+//     this.id = id;
+//     this.status = "unavailable";
+//     this.setPosition(latitude, longitude);
+// }
+// let car = new Vehicle({id:"XE9", latitude: 12.9090909, longitude: 24.4979457})
+// console.log("car:", car)
+// console.log("Lat Long:", car.getPosition())
+// let {latitude, longitude} = car.getPosition()
+// console.log("latitude:", latitude)
+// console.log("longitude:", longitude)
+
+// class Vehicle
+// {
+//     constructor({id, latitude, longitude})
+//     {
+//    		this.id = id;
+//         this.status = "unavailable";
+//    		this.setPosition({latitude, longitude});
+//     };
+//     setPosition({latitude, longitude}) 
+//     {
+//         this.time = Date.now();
+//         this.longitude = longitude;
+//         this.latitude = latitude;
+//     }
+//     getPosition() 
+//     {
+//         return{
+//             latitude: this.latitude,
+//             longitude: this.longitude
+//         };
+//     };
+// };
+// let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
+// vehicle.setPosition({longitude: 18.193121, latitude: 59.378654});
+// console.log(vehicle.getPosition())
+
+function namedFunction() 
+{ 
+    console.log("I'm nj, I hope ..") 
+};
+let anonymousFunction = function() 
 {
-    this.setPosition = function(latitude, longitude) 
-    {
-        this.time = Date.now();
-        this.longitude = longitude;
-        this.latitude = latitude;
-    };
-
-    this.getPosition = function() 
-    {
-        return {latitude: this.latitude, longitude: this.longitude}
-    };
-
-    this.id = id;
-    this.status = "unavailable";
-    this.setPosition(latitude, longitude);
-}
-let car = new Vehicle({id:"XE9", latitude: 12.9090909, longitude: 24.4979457})
-console.log("car:", car)
-console.log("Lat Long:", car.getPosition())
-let {latitude, longitude} = car.getPosition()
-console.log("latitude:", latitude)
-console.log("longitude:", longitude)
+    console.log("I'm a bit anonymous ...")
+};
+let notExactlyAnonymousFunction = function anotherNamedFunction() 
+{
+    console.log("I'm confused ...")
+};
+namedFunction();	// -> I'm named, I hope ...
+anonymousFunction();	// -> I'm a bit anonymous ...
+notExactlyAnonymousFunction();	// -> I'm confused …
