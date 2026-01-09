5ba51a7a0181f16e70e6a31b7549d085caf163e6
Aditya Gupta <adtyagupta72@gmail.com>
2026-01-09 20:03:47 +0530

    Static method

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-sayHello
+Vehicle.isSameId =  function(v1, v2) 
+// {
+//     return v1.id === v2.id
+// }
--- userInteraction.js
+++ userInteraction.js
@@ -2178,22 +2178,70 @@ returns => 5 * 24
 // let obj = new ExtendedClass("Bob")
 // obj.sayHi()    // -> Hi Bob!
 
-class AlmostEmptyClass 
+// class AlmostEmptyClass 
+// {
+//     constructor(sth) 
+//     {
+//         console.log(sth)
+//     }
+//     sayHi() 
+//     {
+//         console.log("Hi!")
+//     }
+//     static sayHello() 
+//     {
+//         console.log("Hello!")
+//     }
+// }
+// let almostEmptyObject = new AlmostEmptyClass(120) // 120
+// almostEmptyObject.sayHi() // -> Hi!
+// //almostEmptyObject.sayHello() // error
+// AlmostEmptyClass.sayHello() // -> Hello!
+
+
+class Vehicle 
 {
-    constructor(sth) 
+    constructor({id, latitude, longitude})
     {
-        console.log(sth)
+        this.id = id
+        this.status = "unavailable"
+        this.setPosition({latitude, longitude})
     }
-    sayHi() 
+    setPosition({latitude, longitude})
     {
-        console.log("Hi!")
+        this.time = Date.now()
+        this.longitude = longitude
+        this.latitude = latitude
     }
-    static sayHello() 
+    getPosition() 
     {
-        console.log("Hello!")
+        return{
+            latitude: this.latitude,
+            longitude: this.longitude
+        }
+    }
+    // static isSameId(v1, v2)
+    // {
+    //     return v1.id === v2.id
+    // }
+
+    static isSameLongitude(v1, v2)
+    {
+        return v1.longitude === v2.longitude
     }
 }
-let almostEmptyObject = new AlmostEmptyClass(120) // 120
-almostEmptyObject.sayHi() // -> Hi!
-//almostEmptyObject.sayHello() // error
-AlmostEmptyClass.sayHello() // -> Hello!
+
+Vehicle.isSameId =  function(v1, v2) 
+{
+    return v1.id === v2.id
+}
+
+
+let vehicle1 = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"})
+let vehicle2 = new Vehicle({longitude: 0, latitude: 0, id: "AL1024"})
+let vehicle3 = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1026"})
+console.log("Vehicle.isSameId(vehicle1, vehicle2)", Vehicle.isSameId(vehicle1, vehicle2)) // -> true
+console.log("Vehicle.isSameId(vehicle1, vehicle3)", Vehicle.isSameId(vehicle1, vehicle3)) // -> false
+console.log("Vehicle.isSameLongitude(vehicle1, vehicle2)", Vehicle.isSameLongitude(vehicle1, vehicle2))
+console.log("Vehicle.isSameLongitude(vehicle1, vehicle3)", Vehicle.isSameLongitude(vehicle1, vehicle3))
+
