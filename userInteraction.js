c201c91b84f7c79d86fb0cf9f0e9a4fb27168920
Aditya Gupta <adityaguptady@gmail.com>
2026-02-03 20:02:26 +0530

    Built in methods Date

--- userInteraction.js
+++ userInteraction.js
@@ -2505,13 +2505,49 @@ returns => 5 * 24
 // console.log("Ørnes".localeCompare("Sundsfjord", "nn")) // -> 1
 // console.log("Ørnes".localeCompare("Brettesnes", "nn")) // -> 1
 
-console.log(Date.now())
-console.log(new Date())
-console.log(new Date().toLocaleString())
-console.log(Date.now().toLocaleString())
+// console.log(Date.now())
+// console.log(new Date())
+// console.log(new Date().toLocaleString())
+// console.log(Date.now().toLocaleString())
 
-let date1 = new Date(0)
-let date2 = new Date(1000*60*60*10)
-console.log(date1.toUTCString()) // -> Thu, 01 Jan 1970 00:00:00 GMT
-console.log(date2.toUTCString()) // -> Thu, 01 Jan 1970 10:00:00 GMT
+// let date1 = new Date(0)
+//  let date2 = new Date(1000*60*60*10)
+// // console.log(date1.toUTCString()) // -> Thu, 01 Jan 1970 00:00:00 GMT
+// // console.log(date2.toUTCString()) // -> Thu, 01 Jan 1970 10:00:00 GMT
 
+// console.log(date2.getTimezoneOffset()) // -> 0
+// console.log(date2.toLocaleString()) // -> 01/01/1970, 10:00:00
+// console.log(date2.toISOString()) // -> 1970-01-01T10:00:00.000Z
+// console.log(date2.toUTCString()) // -> Thu, 01 Jan 1970 10:00:00 GMT
+
+// let date3 = new Date("2026-02-02T20:20:00.000")
+// let date4 = new Date("2026-02-02T20:20:00.000Z")
+// console.log(date3.toLocaleString()) // -> 02/02/2020, 20:20:00
+// console.log(date3.toISOString()) // -> 2020-02-02T19:20:00.000Z
+// console.log(date3.toUTCString()) // -> Sun, 02 Feb 2020 19:20:00 GMT
+
+// console.log(date4.toLocaleString()) // -> 02/02/2020, 21:20:00
+// console.log(date4.toISOString()) // -> 2020-02-02T20:20:00.000Z
+// console.log(date4.toUTCString()) // -> Sun, 02 Feb 2020 20:20:00 GMT
+
+// console.log(date3.getTime()) // -> 1580671200000
+// console.log(date4.getTime()) // -> 1580674800000
+// console.log(date4.getTime() - date3.getTime()) // -> 3600000
+
+// let nowObj = new Date()
+// console.log(nowObj.toLocaleString())
+
+// let now = Date.now() // timestamp
+// let nowObj1 = new Date(now)
+// console.log(`now : ${typeof now} : ${now}`)
+// console.log(`now : ${typeof nowObj1} : ${nowObj1}`)
+
+let date1 = new Date(2020, 6)
+let date2 = new Date(2020, 6, 8)
+let date3 = new Date(2020, 6, 8, 10)
+let date4 = new Date(2020, 6, 8, 10, 20, 45)
+
+console.log(date1.toLocaleString()) // -> 01/07/2020, 00:00:00
+console.log(date2.toLocaleString()) // -> 08/07/2020, 00:00:00
+console.log(date3.toLocaleString()) // -> 08/07/2020, 10:00:00
+console.log(date4.toLocaleString()) // -> 08/07/2020, 10:20:45
